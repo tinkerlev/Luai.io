64e4d86137e425cf40f79a29c24b3c2eb5f37a76
Loai Eliran Deeb <elirandeeb@gmail.com>
2025-07-23 16:38:52 -0300

    Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Analytics } from "@vercel/analytics/next"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { LanguageProvider } from './contexts/LanguageContext';
