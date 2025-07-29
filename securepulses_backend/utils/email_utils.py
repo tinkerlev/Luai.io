47dc7bdbcd4e5f447b158a7f48fdece55fbfe273
tinkerlev <elirandeeb@gmail.com>
2025-07-29 10:46:55 -0300

    bla

--- securepulses_backend/utils/email_utils.py
+++ securepulses_backend/utils/email_utils.py
@@ -7,6 +7,7 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from dotenv import load_dotenv
 import pathlib
+from utils.contact_email import generate_contact_thank_you_html
 
 load_dotenv()
 
@@ -37,6 +38,21 @@ def send_admin_notification(name: str, email: str, company: str, message: str):
     msg.set_content(body)
 
     _send_email(msg, SMTP_USER, SMTP_PASS)
+
+def send_user_confirmation(name: str, email: str, company: str, message: str):
+    """
+    Send confirmation email to the user using the email.html template.
+    """
+    subject = "We Received Your Security Assessment Request"
+    html_body = generate_contact_thank_you_html(name, company, message)
+    msg = EmailMessage()
+    msg["From"] = formataddr((SENDER_NAME, NOREPLY_EMAIL))
+    msg["To"] = email
+    msg["Subject"] = subject
+    msg.set_content("Thank you for contacting us. We received your request and will get back to you soon.", subtype="plain")
+    msg.add_alternative(html_body, subtype="html")
+
+    _send_email(msg, NOREPLY_SMTP_USER or SMTP_USER, NOREPLY_SMTP_PASS or SMTP_PASS)
 
 def _send_email(msg: EmailMessage, smtp_user: str, smtp_pass: str):
     try:
