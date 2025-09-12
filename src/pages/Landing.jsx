5907bc62688c81d3aaf3f4ebcf9114a8362c8fd2
tinkerlev <elirandeeb@gmail.com>
2025-09-12 19:34:57 -0300

    a

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Analytics } from "@vercel/analytics/react"
 import { Shield, AlertTriangle, TrendingUp } from 'lucide-react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import emailjs from '@emailjs/browser';
