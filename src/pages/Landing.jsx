d612c698a3e587c8d9ad612cd2a5edb6e70fc455
tinkerlev <elirandeeb@gmail.com>
2025-09-12 16:26:38 -0300

    fix

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,6 +4,7 @@ import { Shield, AlertTriangle, TrendingUp } from 'lucide-react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import emailjs from '@emailjs/browser';
+import PageSEO from '../components/PageSEO';
 import {
   GradientHeading,
   PrimaryButton,
